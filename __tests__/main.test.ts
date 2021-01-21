bd25c2b7abe7e364ea0845fe10913096d2540e36
Koala Yeung <koalay@gmail.com>
2021-01-21 19:36:54 +0800

    wip

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,15 +2,43 @@ jest.mock('@actions/core');
 jest.mock('@actions/github');
 jest.mock('fs');
 
-import core from '@actions/core'
+const core = require('@actions/core')
 import run from '../src/functions'
+
 
 // Frankly, tests would be entirely useless unless we can mock GitHub somehow.
 describe('Upload Release Action', () => {
+  let uploadReleaseAsset: any
   let spyGetOctokit: jest.SpyInstance<any>
   let mockOctokit: any
 
   beforeEach(() => {
+    uploadReleaseAsset = jest.fn().mockReturnValueOnce({
+      data: {
+        browser_download_url: 'browserDownloadUrl'
+      }
+    })
+
+    fs.statSync = jest.fn().mockReturnValueOnce({
+      size: 527
+    })
+
+    content = Buffer.from('test content')
+    fs.readFileSync = jest.fn().mockReturnValueOnce(content)
+
+    context.repo = {
+      owner: 'owner',
+      repo: 'repo'
+    }
+
+    const github = {
+      repos: {
+        uploadReleaseAsset
+      }
+    }
+
+    GitHub.mockImplementation(() => github);
+
     mockOctokit = {
       repos: {
         createRelease: jest.fn(async () => ({
@@ -25,31 +53,38 @@ describe('Upload Release Action', () => {
     // jest.spyOn(github, "context", "get").mockImplementation(() => "testtest");
   })
 
-  it('pls write actual test', async () => {
+  it('Upload release asset with only the required inputs', async () => {
     core.getInput = jest
       .fn()
-      // repo_token
+      // repo_token (required)
       .mockReturnValueOnce('repo_token')
-      // file
-      .mockReturnValueOnce('file')
-      // tag
+      // file (required)
+      .mockReturnValueOnce('filename')
+      // tag (required)
       .mockReturnValueOnce('tag')
       // file_glob
-      .mockReturnValueOnce('false')
+      .mockReturnValueOnce('')
       // overwrite
-      .mockReturnValueOnce('false')
+      .mockReturnValueOnce('')
       // prerelese
-      .mockReturnValueOnce('false')
+      .mockReturnValueOnce('')
       // release_name
-      .mockReturnValueOnce('release_name')
+      .mockReturnValueOnce('')
       // body
-      .mockReturnValueOnce('body')
+      .mockReturnValueOnce('')
       // asset_name
-      .mockReturnValueOnce('asset_name')
+      .mockReturnValueOnce('')
       // asset_name (again)
-      .mockReturnValueOnce('asset_name')
+      .mockReturnValueOnce('')
 
     await run();
+
+    expect(uploadReleaseAsset).toHaveBeenCalledWith({
+      url: 'upload_url',
+      headers: { 'content-type': 'binary/octet-stream', 'content-length': 527 },
+      name: 'filename',
+      file: content
+    });
 
   })
 })
